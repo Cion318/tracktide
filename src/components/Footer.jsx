49a2a6c528738d5ebd5b5c257db4b787ca36af7f
Alexander Avercenko <alexander.avercenko@gmail.com>
2023-12-05 16:54:54 +0100

    Created Footer.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import faTailwind from "../assets/faTailwind.png";
+import faVite from "../assets/faVite.svg";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-end">
+      <h1 className="text-sm text-dimWhite">Created with</h1>
+      <img className="w-6 pl-1" src={faVite} />
+      <FontAwesomeIcon className="pl-1 text-xl" icon={faReact} />
+      <img className="pl-1 text-xl" src={faTailwind} />
+    </div>
+  );
+};
+
+export default Footer;
