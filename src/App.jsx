ed7cbfabd6b86ae5a45cde9ef4929404f5e54c6d
Alexander Avercenko <alexander.avercenko@gmail.com>
2023-12-06 15:10:44 +0100

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import "./index.css";
@@ -7,14 +7,14 @@ import Main from "./pages/Main";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/main" element={<Main />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
